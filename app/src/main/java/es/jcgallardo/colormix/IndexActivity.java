82d8305d2e3a72006c5e627dd7b188e0ae5a775d
jcgallardo <jcgallardomorales@gmail.com>
2017-08-21 19:56:41 +0200

    Pantalla index creada

--- app/src/main/java/es/jcgallardo/colormix/IndexActivity.java
+++ app/src/main/java/es/jcgallardo/colormix/IndexActivity.java
@@ -1,7 +1,8 @@
 package es.jcgallardo.colormix;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class IndexActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        // Paramos la ejecución 5s
+        //SystemClock.sleep(5000);
+
+        //Una vez despierto cambiamos a la pantalla de bienvenida
+        Intent Welcome = new Intent(getApplicationContext(), WelcomeActivity.class);
+        //startActivity(Welcome);
+
     }
 }
---
+++ app/src/main/java/es/jcgallardo/colormix/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package es.jcgallardo.colormix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}

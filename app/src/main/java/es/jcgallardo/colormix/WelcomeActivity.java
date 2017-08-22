001572fa2e9c7a083ea607f24c2783549f17ad1c
jcgallardo <jcgallardomorales@gmail.com>
2017-08-22 21:37:36 +0200

    Pantalla de Bienvenida y primer acercamiento al MainActivity donde estará toda la funcionalidad de la aplicación. Creación de un menú inferior.

--- app/src/main/java/es/jcgallardo/colormix/WelcomeActivity.java
+++ app/src/main/java/es/jcgallardo/colormix/WelcomeActivity.java
@@ -1,11 +1,19 @@
 package es.jcgallardo.colormix;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
-    ImageView paint1;
+    TextView tv_color;
+    Button b_comenzar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +21,26 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         // instanciación
-        //paint1 = (ImageView) findViewById(R.id.paint1);
-        //paint1.setColorFilter(Color.rgb(200, 0, 0));
+        tv_color = (TextView) findViewById(R.id.texto_color);
+        b_comenzar = (Button) findViewById(R.id.b_comenzar);
 
+        // crear color gradiente
+
+        Shader myShader = new LinearGradient(
+                0, 0, 800, 800,
+                Color.rgb(237,88,56),
+                Color.rgb(247,228,168),
+                Shader.TileMode.CLAMP );
+
+        tv_color.getPaint().setShader(myShader);
+        tv_color.setGravity(Gravity.CENTER);
+
+        b_comenzar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
 
     }
 }

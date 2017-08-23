e93d3030bb1a4cb4478c9acf2cde9bc5e58e4207
jcgallardo <jcgallardomorales@gmail.com>
2017-08-23 21:56:03 +0200

    Menú de navegación inferior (bottom navegation menu) para la pantalla principal terminado. Ya se cambian los colores al pasar el foco.

--- app/src/main/java/es/jcgallardo/colormix/MainActivity.java
+++ app/src/main/java/es/jcgallardo/colormix/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        navigation.setSelectedItemId(R.id.navigation_principal);
+        //navigation.setSelectedItemId(R.id.navigation_principal);
     }
 
 }
